0fbae34462a749ddfcdf7f3d96ff71272f3f5c79
jieun <s2ljeun@gmail.com>
2022-06-07 16:47:59 +0900

    useEffect()

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
   useEffect(() => { //익명의 함수
-    console.log("CALL THE API");
+    console.log("I run only once.");
   }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >5) { //컴포넌트가 처음 로딩될 때는 실행 X
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]); //[]안의 state가 변화할 때만 코드 실행, counter 변화할 때 X
+
+  useEffect(() => {
+    if (counter !== 0) {
+      console.log("I run when 'counter' changes.");
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    if (counter !== 0 && keyword !== "") {
+      console.log("I run when 'counter' & 'keyword' changes.");
+    }
+  }, [counter, keyword]);
+
   return (
     <div>
       <input value={keyword} onChange={onChange} type="text" placeholder='Search here...' />
@@ -19,19 +37,3 @@ function App() {
 }
 
 export default App;
-
-// useEffect()
-// 특정 코드가 단 한번만 실행될 수 있게 보호하는 역할(state가 변경되어도)
-
-// state prop effect
-// * create-react-app을 사용하기 때문에
-// import { useState, useEffect } from "react";
-// React.useState() -> useState()
-
-
-// const [keyword, setKeyword] = useState("");
-// const onChange = (event) => setKeyword(event.target.value); //event 타겟의 value를 받아 setKeyword에 넣음
-
-// <input value={keyword} onChange={onChange} type="text" placeholder='Search here...' />
-
-// onChange event 감지 -> Function onChange 실행 -> setKeyword() 실행 -> input value에 넣음
