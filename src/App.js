8cce78c3c83053bf4b5e1c804d5f9e25b2638b44
jieun <s2ljeun@gmail.com>
2022-06-07 17:32:43 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { useState, useEffect } from "react";
 
-function Hello(){
-  useEffect(() => {
-    console.log("Hi :)");
-    return () => console.log("Bye :(");
-  }, [])
-   // Cleanup fn: component가 destroy될 때, useEffect가 return한 함수를 실행해 준다
-  return <h1>Hello</h1>;
-} // 컴포넌트는 jsx를 return하는 function일 뿐
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
-    </div>
-  );
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return; //함수 작동 X
+    }
+    setToDo("");
+  };
+
+return <div>
+    <form onSubmit={onSubmit}>
+      <input
+        onChange={onChange}
+        value={toDo}
+        type="text"
+        placeholder='할 일을 입력하세요.'
+      />
+      <button>할 일 추가</button>
+    </form>
+  </div>
 }
 
 export default App;
