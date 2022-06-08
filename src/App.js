9b6e0431b2050dfb25392d0da67cccb35eff8e2d
jieun <s2ljeun@gmail.com>
2022-06-08 20:24:50 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") {
-      return; //함수 작동 X
+      return; //투두가 비어있다면 함수 작동 X
     }
+    setToDos((currentArray) => [toDo, ...currentArray]); // currentArray의 모든 요소를 불러온다 - array 안에 array를 추가하는 대신
     setToDo("");
   };
 
-return <div>
+return (
+  <div>
+    <h1>할 일({toDos.length})</h1>
     <form onSubmit={onSubmit}>
       <input
         onChange={onChange}
@@ -21,7 +25,12 @@ return <div>
       />
       <button>할 일 추가</button>
     </form>
+    <hr />
+    <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
   </div>
+  )
 }
 
 export default App;
