18afb472c2fcde8738e462a329b99c10ca766724
s2ljeun <s2ljeun@gmail.com>
2022-06-14 22:29:17 +0900

    coin tracker code challenging

--- src/coin.js
+++ src/coin.js
@@ -11,18 +11,39 @@ function App() {
       setLoading(false);
   });
 }, []); // 로딩 직후 1회만 코드 실행
+  const [budget, setBudget] = useState("");
+  const [amount, setAmount] = useState("");
+  const onChange = (event) => {
+    setBudget(event.target.value);
+  }
+  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setAmount(budget);
+    setBudget("");
+  }
+
+  const selectCoin = () => {
+    console.log("coin changed!");
+  }
 
 return(
   <div>
     <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
     {loading ? (<strong>Loading...</strong>
     ) : (
-    <select>
-      {coins.map((coin) =>
-        <option>
-          {coin.name} ({coin.symbol}:{coin.quotes.USD.price} USD)
-        </option>)}
-    </select>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input value={budget} onChange={onChange} placeholder='BUDGET / USD'></input>
+        </form>
+        <select onChange={selectCoin}>
+          {coins.map((coin) =>
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}:{coin.quotes.USD.price} USD)
+            </option>)}
+        </select>
+        <h2>You can buy <span>{amount}</span></h2>
+      </div>
     )}
   </div>
   )
